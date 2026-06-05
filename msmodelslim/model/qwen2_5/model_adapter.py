784ff97c127bef8311b021605a64478fcc481308
code_mingming <limingyu35@h-partners.com>
2026-06-05 17:56:43 +0800

    [pre-commit]fix pre-commit for qwen2.5
    Co-authored-by: 李明宇<limingyu35@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !501 merge pre-commit-for-qwen25 into master
    
    [pre-commit]fix pre-commit for qwen2.5
    
    Created-by: code_mingming
    Commit-by: 李明宇
    Merged-by: ascend-robot
    Description: 感谢您贡献的Pull Request！
    在提交之前，请务必阅读 [CONTRIBUTING.md](https://gitcode.com/Ascend/msmodelslim/blob/master/CONTRIBUTING.md)。
    
    Thanks for sending a pull request!
    BEFORE SUBMITTING, PLEASE READ [CONTRIBUTING.md](https://gitcode.com/Ascend/msmodelslim/blob/master/CONTRIBUTING.md).
    
    ## PR描述 (What this PR does / why we need it?)
    fix pre-commit for qwen2.5
    
    ## 面向用户的变更 (Does this PR introduce _any_ user-facing change)?
    - 请注意，这里指的是**任何**面向用户的变更，包括但不限于API、用户界面或其他使用方式上的变更。
    
    - Note that it means *any* user-facing change including all aspects such as API, interface or other behavior changes.
    
    
    
    ## 功能验证 (How was this patch tested?)
    
    请确认CI已通过增量及存量的单元测试用例。
    如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤(最好提供完整的可复现的操作路径及关键截图)，以便Committer能够快速复现验证，也便于后续的维护。
    如果未添加测试，请说明未添加的原因，以及为何难添加测试。
    
    - [_] 功能自验
    - [_] 本地自验截图(涉及个人标识符等敏感信息请注意脱敏)
    - [_] 新增/变更内容是否已新增/适配UT测试用例看护
    
    CI passed with new added/existing test.
    If it was tested in a way different from regular unit tests, please clarify how you tested step by step, ideally copy and paste-able, so that other reviewers can test and check, and descendants can verify in the future.
    If tests were not added, please describe why they were not added and/or why it was difficult to add.
    
    - [_] Self-verification of the feature.
    - [_] Screenshot of local self-verification (please anonymize any sensitive information such as personal identifiers)
    - [_] Have new or modified unit test (UT) cases been added or adapted to cover the newly added or changed content?
    
    
    See merge request: Ascend/msmodelslim!501

--- msmodelslim/model/qwen2_5/model_adapter.py
+++ msmodelslim/model/qwen2_5/model_adapter.py
@@ -32,18 +32,24 @@ from msmodelslim.utils.logging import logger_setter
 from msmodelslim.utils.security.model import SafeGenerator
 from ..common.layer_wise_forward import generated_decoder_layer_visit_func, transformers_generated_forward_func
 from ..default.model_adapter import DefaultModelAdapter
-from ..interface_hub import ModelInfoInterface, ModelSlimPipelineInterfaceV0, ModelSlimPipelineInterfaceV1, \
-    AnalyzePipelineInterface, KVSmoothFusedInterface
+from ..interface_hub import (
+    ModelInfoInterface,
+    ModelSlimPipelineInterfaceV0,
+    ModelSlimPipelineInterfaceV1,
+    AnalyzePipelineInterface,
+    KVSmoothFusedInterface,
+)
 
 
 @logger_setter()
-class Qwen25ModelAdapter(DefaultModelAdapter,
-                         ModelInfoInterface,
-                         ModelSlimPipelineInterfaceV0,
-                         ModelSlimPipelineInterfaceV1,
-                         AnalyzePipelineInterface,
-                         KVSmoothFusedInterface,
-                         ):
+class Qwen25ModelAdapter(  # pylint: disable=too-many-ancestors
+    DefaultModelAdapter,
+    ModelInfoInterface,
+    ModelSlimPipelineInterfaceV0,
+    ModelSlimPipelineInterfaceV1,
+    AnalyzePipelineInterface,
+    KVSmoothFusedInterface,
+):
     def get_model_type(self) -> str:
         return self.model_type
 
@@ -56,13 +62,8 @@ class Qwen25ModelAdapter(DefaultModelAdapter,
     def handle_dataset(self, dataset: Any, device: DeviceType = DeviceType.NPU) -> List[Any]:
         return self._get_tokenized_data(dataset, device)
 
-    def handle_dataset_by_batch(self,
-                                dataset: Any,
-                                batch_size: int,
-                                device: DeviceType = DeviceType.NPU) -> List[Any]:
-        return self._get_batch_tokenized_data(calib_list=dataset,
-                                              batch_size=batch_size,
-                                              device=device)
+    def handle_dataset_by_batch(self, dataset: Any, batch_size: int, device: DeviceType = DeviceType.NPU) -> List[Any]:
+        return self._get_batch_tokenized_data(calib_list=dataset, batch_size=batch_size, device=device)
 
     def init_model(self, device: DeviceType = DeviceType.NPU) -> nn.Module:
         return self._load_model(device)
@@ -70,8 +71,11 @@ class Qwen25ModelAdapter(DefaultModelAdapter,
     def generate_model_visit(self, model: nn.Module) -> Generator[ProcessRequest, Any, None]:
         yield from generated_decoder_layer_visit_func(model)
 
-    def generate_model_forward(self, model: nn.Module, inputs: Any,
-                               ) -> Generator[ProcessRequest, Any, None]:
+    def generate_model_forward(
+        self,
+        model: nn.Module,
+        inputs: Any,
+    ) -> Generator[ProcessRequest, Any, None]:
         yield from transformers_generated_forward_func(model, inputs)
 
     def enable_kv_cache(self, model: nn.Module, need_kv_cache: bool) -> None:
@@ -84,39 +88,48 @@ class Qwen25ModelAdapter(DefaultModelAdapter,
                 layer_idx=i,
                 fused_from_query_states_name="q_proj",
                 fused_from_key_states_name="k_proj",
-                fused_type=KVSmoothFusedType.StateViaRopeToLinear
+                fused_type=KVSmoothFusedType.StateViaRopeToLinear,
             )
             for i in range(self.config.num_hidden_layers)
         ]
 
     def get_head_dim(self) -> int:
         if not hasattr(self.config, 'hidden_size'):
-            raise InvalidModelError("hidden_size is not found in config.json",
-                                    action="Please check the model config.json")
+            raise InvalidModelError(
+                "hidden_size is not found in config.json", action="Please check the model config.json"
+            )
         if not hasattr(self.config, 'num_attention_heads'):
-            raise InvalidModelError("num_attention_heads is not found in config.json",
-                                    action="Please check the model config.json")
+            raise InvalidModelError(
+                "num_attention_heads is not found in config.json", action="Please check the model config.json"
+            )
         if self.config.num_attention_heads == 0:
-            raise InvalidModelError("num_attention_heads is 0 in config.json, which should be greater than 0",
-                                    action="Please check the model config.json")
+            raise InvalidModelError(
+                "num_attention_heads is 0 in config.json, which should be greater than 0",
+                action="Please check the model config.json",
+            )
         return self.config.hidden_size // self.config.num_attention_heads
 
     def get_num_key_value_groups(self) -> int:
         if not hasattr(self.config, 'num_attention_heads'):
-            raise InvalidModelError("num_attention_heads is not found in config.json",
-                                    action="Please check the model config.json")
+            raise InvalidModelError(
+                "num_attention_heads is not found in config.json", action="Please check the model config.json"
+            )
         if not hasattr(self.config, 'num_key_value_heads'):
-            raise InvalidModelError("num_key_value_heads is not found in config.json",
-                                    action="Please check the model config.json")
+            raise InvalidModelError(
+                "num_key_value_heads is not found in config.json", action="Please check the model config.json"
+            )
         if self.config.num_key_value_heads == 0:
-            raise InvalidModelError("num_key_value_heads is 0 in config.json, which should be greater than 0",
-                                    action="Please check the model config.json")
+            raise InvalidModelError(
+                "num_key_value_heads is 0 in config.json, which should be greater than 0",
+                action="Please check the model config.json",
+            )
         return self.config.num_attention_heads // self.config.num_key_value_heads
 
     def get_num_key_value_heads(self) -> int:
         if not hasattr(self.config, 'num_key_value_heads'):
-            raise InvalidModelError("num_key_value_heads is not found in config.json",
-                                    action="Please check the model config.json")
+            raise InvalidModelError(
+                "num_key_value_heads is not found in config.json", action="Please check the model config.json"
+            )
         return self.config.num_key_value_heads
 
     def _load_tokenizer(self, trust_remote_code=False) -> PreTrainedTokenizerBase:
@@ -125,6 +138,7 @@ class Qwen25ModelAdapter(DefaultModelAdapter,
             use_fast=False,
             legacy=False,
             padding_side='left',
-            pad_token='<|extra_0|>',
-            eos_token='<|endoftext|>',
-            trust_remote_code=trust_remote_code)
+            pad_token='<|extra_0|>',  # nosec B106
+            eos_token='<|endoftext|>',  # nosec B106,
+            trust_remote_code=trust_remote_code,
+        )
